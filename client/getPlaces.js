51375058506e2ff8ff7193aecfa925cf4a8d4573
Chiajun <chiajun@iastate.edu>
2016-02-20 14:01:47 -0600

    Changees in googleMap

--- client/getPlaces.js
+++ client/getPlaces.js
@@ -2,12 +2,20 @@ Markers = new Mongo.Collection('markers');
 
 if (Meteor.isClient) {
     var MAP_ZOOM = 15;
-
+    var resNames;
     Meteor.startup(function() {
         GoogleMaps.load({key: 'AIzaSyALQD2Ax84rd5q_MYLZ-AbWpE17F8fE0-E', libraries: 'places' });
+
+        Meteor.methods({
+            updateResNames: function(names){
+                resNames = names;
+            },
+
+            getResNames: function(){
+                return resNames;
+            }
+        });
     });
-
-
 
     Template.map.helpers({
         geolocationError: function() {
@@ -23,11 +31,43 @@ if (Meteor.isClient) {
                     zoom: MAP_ZOOM
                 };
             }
+        },
+
+        getPlacesInfo: function() {
+            GoogleMaps.ready('map', function(map) {
+                var userLocation = Geolocation.latLng();
+                var m = new google.maps.Map(document.getElementById('gmap'), {
+                    center: userLocation,
+                    zoom: 15
+                });
+
+                var service = new google.maps.places.PlacesService(m).nearbySearch({
+                    location: userLocation,
+                    radius: 5000,
+                    types: ['restaurant']
+                }, callback);
+
+                function callback(results, status) {
+                    if (status === google.maps.places.PlacesServiceStatus.OK) {
+                        Meteor.call('updatesResNames',results);
+                        return JSON.stringify(getPlaces(results));
+                    }
+                }
+
+                function getPlaces(results){
+                    var names = [];
+                    for(i = 0; i < results.length; i++){
+                        names.push(results[i].name);
+                    }
+
+                    return names;
+                }
+            });
         }
+
     });
 
     Template.map.onCreated(function() {
-
         GoogleMaps.ready('map', function(map) {
             var userLocation = Geolocation.latLng();
             var marker = new google.maps.Marker({
@@ -38,7 +78,7 @@ if (Meteor.isClient) {
             var m = new google.maps.Map(document.getElementById('gmap'), {
                 center: userLocation,
                 zoom: 15
-            });;
+            });
 
             var service = new google.maps.places.PlacesService(m);
             var infowindow = new google.maps.InfoWindow();
@@ -50,12 +90,8 @@ if (Meteor.isClient) {
 
             function callback(results, status) {
                 if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    for (var i = 0; i < results.length; i++) {
-                        createMarker(results[i]);
-                    }
+                    //return console.log(JSON.stringify(getPlaces(results)));
                 }
-
-                console.log(getPlaces(results));
             }
 
             function getPlaces(results){
@@ -78,13 +114,35 @@ if (Meteor.isClient) {
         });
     });
 
+    //Template.resNames.helpers({
+    //    getPlacesInfo: function() {
+    //        if(GoogleMaps.loaded()){
+    //            var m = new google.maps.Map(document.getElementById('gmap'), {
+    //                center: userLocation,
+    //                zoom: 15
+    //            });
+    //
+    //            var service = new google.maps.places.PlacesService(m);
+    //            service.nearbySearch({
+    //                location: userLocation,
+    //                radius: 5000,
+    //                types: ['restaurant']
+    //            }, callback);
+    //
+    //            function callback(results, status) {
+    //                if (status === google.maps.places.PlacesServiceStatus.OK) {
+    //                    return JSON.stringify(getPlaces(results));
+    //                }
+    //            }
+    //        }
+    //    }
+    //});
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
         // code to run on server at startup
-
     });
-
-
 }
+
+
