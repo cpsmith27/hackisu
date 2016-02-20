05b2a72697aa8d31b9442e20a67c508e8fa33af1
Chiajun <chiajun@iastate.edu>
2016-02-20 03:28:40 -0600

    Fixing google apis

--- client/getPlaces.js
+++ client/getPlaces.js
@@ -1,47 +1,68 @@
+Markers = new Mongo.Collection('markers');
+
+
 if (Meteor.isClient) {
+    var MAP_ZOOM = 15;
+
     Meteor.startup(function() {
-        GoogleMaps.load();
+        GoogleMaps.load({key: 'AIzaSyALQD2Ax84rd5q_MYLZ-AbWpE17F8fE0-E', libraries: 'places' });
     });
 
-    Template.body.helpers({
-        exampleMapOptions: function() {
-            // Make sure the maps API has loaded
-            if (GoogleMaps.loaded()) {
-                // Map initialization options
+
+
+    Template.map.helpers({
+        geolocationError: function() {
+            var error = Geolocation.error();
+            return error && error.message;
+        },
+        mapOptions: function() {
+            var userLocation = Geolocation.latLng();
+            // Initialize the map once we have the latLng.
+            if (GoogleMaps.loaded() && userLocation) {
                 return {
-                    center: new google.maps.LatLng(-37.8136, 144.9631),
-                    zoom: 12
+                    center: new google.maps.LatLng(userLocation.lat, userLocation.lng),
+                    zoom: MAP_ZOOM
                 };
             }
         }
     });
 
-    Template.body.onCreated(function() {
-        // We can use the `ready` callback to interact with the map API once the map is ready.
-        GoogleMaps.ready('exampleMap', function(map) {
-            // Add a marker to the map once it's ready
+    Template.map.onCreated(function() {
+
+        GoogleMaps.ready('map', function(map) {
+            var userLocation = Geolocation.latLng();
+
             var marker = new google.maps.Marker({
                 position: map.options.center,
                 map: map.instance
             });
 
-            if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-                    var pos = {
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    };
+            console.log(map);
 
-                    infoWindow.setPosition(pos);
-                    infoWindow.setContent('Location found.');
-                    map.setCenter(pos);
-                }, function() {
-                    handleLocationError(true, infoWindow, map.getCenter());
-                });
-            } else {
-                // Browser doesn't support Geolocation
-                console.log("Problem!");
+            var service = new google.maps.places.PlacesService(map);
+
+            service.nearbySearch({
+                location: userLocation,
+                radius: 5000,
+                types: ['restaurant']
+            }, callback);
+
+            function callback(results, status) {
+                if (status === google.maps.places.PlacesServiceStatus.OK) {
+                    for (var i = 0; i < results.length; i++) {
+                        createMarker(results[i]);
+                    }
+                }
             }
+
+            function createMarker(place) {
+                var placeLoc = place.geometry.location;
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(placeLoc.lat, placeLoc.lng),
+                    map: map.instance
+                });
+            }
+
         });
     });
 }
@@ -51,4 +72,6 @@ if (Meteor.isServer) {
         // code to run on server at startup
 
     });
+
+
 }
