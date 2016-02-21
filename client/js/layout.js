05c96181442e015600ea2808fe5e5cf5efefb78f
Le Yang Loh <leyang@iastate.edu>
2016-02-20 21:10:42 -0600

    init variables

--- client/js/layout.js
+++ client/js/layout.js
@@ -2,20 +2,35 @@
  * Created by leyang on 2/20/16.
  */
 Template.categories.events({
-    "change .item-toggle": function(e){
+    "toggle .item-toggle": function(e){
         Session.set(e.target.id, e.target.value);
     },
-    "change .range": function(e){
+    "toggle .range": function(e){
         Session.set(e.target.id, e.target.value);
     }
 });
 
+Template.categories.onRendered(function(){
+    var toggle = this.findAll('.item-toggle input');
+    var range = this.findAll('.range input');
+
+    toggle.forEach(function(data){
+        Session.set(data.id, data.value);
+    });
+
+    range.forEach(function(data){
+        Session.set(data.id, data.value);
+    });
+
+});
+
 Template.categories.helpers({
     toggleSelect: function () {
-        return ["Fast Food", "Casual", "Fine Dining", "Special"]
+        return [{name:"Fast Food", color:"assertive"}, {name:"Casual", color:"energized"}, {name:"Fine Dining", color:"balanced"}, {name:"Special", color:"positive"}]
     },
     rangeSelect: function (){
         return ["ion-social-usd", "ion-ios-navigate", "ion-ios-star"]
     }
 });
+
 
