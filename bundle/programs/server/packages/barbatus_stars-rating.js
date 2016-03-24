a2a01c6fc602b5df05b2ca20a817dae89052ea44
Chiajun <chiajun@iastate.edu>
2016-03-24 11:45:54 -0500

    Init commit

---
+++ bundle/programs/server/packages/barbatus_stars-rating.js
@@ -0,0 +1,14 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package['barbatus:stars-rating'] = {};
+
+})();
+
+//# sourceMappingURL=barbatus_stars-rating.js.map
